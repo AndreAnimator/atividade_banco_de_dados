bb146c20f221b721163ec0178838f95988adb82d
AndreAnimator <98904347+AndreAnimator@users.noreply.github.com>
2023-06-23 08:53:30 -0300

    última aula, request exception e etc

--- meusgastos/src/main/java/com/example/meusgastos/domain/service/UsuarioService.java
+++ meusgastos/src/main/java/com/example/meusgastos/domain/service/UsuarioService.java
@@ -1,25 +1,41 @@
 package com.example.meusgastos.domain.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.meusgastos.domain.dto.UsuarioRequestDTO;
 import com.example.meusgastos.domain.dto.UsuarioResponseDTO;
+import com.example.meusgastos.domain.exception.ResourceNotFoundException;
+import com.example.meusgastos.domain.model.Usuario;
+import com.example.meusgastos.domain.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService implements ICRUDService<UsuarioRequestDTO, UsuarioResponseDTO>{
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public List<UsuarioResponseDTO> obterTodos() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'obterTodos'");
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        return usuarios.stream().map(usuario -> mapper.map(usuario, UsuarioResponseDTO.class)).collect(Collectors.toList());
     }
 
     @Override
     public UsuarioResponseDTO obterPorId(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'obterPorId'");
+        Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+        if(optUsuario.isEmpty()){
+            throw new ResourceNotFoundException("Não foi possível encontrar o usuário com o id: " + id);
+        }
+        return mapper.map(optUsuario.get(), UsuarioResponseDTO.class);
     }
 
     @Override
